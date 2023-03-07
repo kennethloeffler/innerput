e454b6102ea7a4848ea55efe7580845618987d1d
Kenneth Loeffler <kenloef@gmail.com>
2023-03-07 12:21:04 -0800

    Support function keys up to F12 in Windows keyboard

--- src/win32/keyboard.rs
+++ src/win32/keyboard.rs
@@ -34,6 +34,18 @@ const VK_RIGHT: u16 = 0x27;
 const VK_UP: u16 = 0x26;
 const VK_DOWN: u16 = 0x28;
 const VK_DELETE: u16 = 0x2E;
+const VK_F1: u16 = 0x70;
+const VK_F2: u16 = 0x71;
+const VK_F3: u16 = 0x72;
+const VK_F4: u16 = 0x73;
+const VK_F5: u16 = 0x74;
+const VK_F6: u16 = 0x75;
+const VK_F7: u16 = 0x76;
+const VK_F8: u16 = 0x77;
+const VK_F9: u16 = 0x78;
+const VK_F10: u16 = 0x79;
+const VK_F11: u16 = 0x7A;
+const VK_F12: u16 = 0x7B;
 
 fn keycode_from_char(char_as_string: String) -> Result<WORD, Error> {
     let buf = &mut [0; 2];
@@ -73,6 +85,18 @@ fn get_keycode(key: &Key) -> Result<WORD, Error> {
         Key::Super => VK_LWIN,
         Key::Tab => VK_TAB,
         Key::Up => VK_UP,
+        Key::F1 => VK_F1,
+        Key::F2 => VK_F2,
+        Key::F3 => VK_F3,
+        Key::F4 => VK_F4,
+        Key::F5 => VK_F5,
+        Key::F6 => VK_F6,
+        Key::F7 => VK_F7,
+        Key::F8 => VK_F8,
+        Key::F9 => VK_F9,
+        Key::F10 => VK_F10,
+        Key::F11 => VK_F11,
+        Key::F12 => VK_F12,
     })
 }
 
